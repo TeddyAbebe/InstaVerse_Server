53735fa42e06a0d67cd65ed6e788f6933fd11dcd
Teddy Abebe <teddyab729@gmail.com>
2023-06-20 23:03:04 +0300

    Update

--- controllers/stories.js
+++ controllers/stories.js
@@ -52,17 +52,25 @@ const deleteStory = async (req, res) => {
 const likeStory = async (req, res) => {
   const { id } = req.params;
 
+  if (!req.userId) return res.json({ message: "Unauthenticated User" });
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).send("This id does not belong to any story");
   }
 
   const story = await Story.findById(id);
 
-  const updatedStory = await Story.findByIdAndUpdate(
-    id,
-    { likes: story.likes + 1 },
-    { new: true }
-  );
+  const index = story.likes.findIndex((id) => id === String(req.userId));
+
+  if (index === -1) {
+    // if user has not liked the story
+    story.likes.push(req.userId);
+  } else {
+    story.likes = story.likes.filter((id) => id !== String(req.userId));
+  }
+
+  const updatedStory = await Story.findByIdAndUpdate(id, story, { new: true });
+
   res.json(updatedStory);
 };
 
